aa6e9d08b299ddd72e2ee4b6961d52d4e0b71968
alizunega <alizunega84@gmail.com>
2024-06-06 10:55:43 -0300

    estructura base proyecto

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 const express = require("express");
-const { publicDecrypt } = require("node:crypto");
+
 const app = express();
 const PORT = 8080;
-const http = require("node:http");
-const os = require("node:os");
+const fs = require("fs");
+
+const http = require("http");
+const os = require("os");
+
+const pathFile = "./public/index.html";
+const html = fs.readFileSync(pathFile);
+
 // const url = "https://osvaldovarela.github.io/tienda_SweetMoon/";
 
 app.use(express.static("public"));
 
 app.get("/productos", (req, res) => {
-  res.sendFile(__dirname + "./productos.html");
+  res.end(html);
 });
 
 app.listen(PORT, () => {
